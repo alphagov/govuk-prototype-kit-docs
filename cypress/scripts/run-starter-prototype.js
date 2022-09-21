5094d1ae4d76d146800348034f1985f3b8c48e3d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-09-21 14:22:18 +0100

    Ensure tests install 0.0.1-alpha.3 in test prototype
    In commit 5c4410c we tried to make sure that the tests for this repo
    would use the use a specific version of the kit, so that the docs tests
    are predictable.
    
    This commit adds the desired version to the command line for the kit
    install script, because otherwise it will default to the latest
    published version.

--- cypress/scripts/run-starter-prototype.js
+++ cypress/scripts/run-starter-prototype.js
@@ -7,7 +7,8 @@ const defaultKitPath = path.join(os.tmpdir(), 'cypress/temp/test-project')
 
 const testDir = path.resolve(process.env.KIT_TEST_DIR || defaultKitPath)
 
-const kitModule = 'govuk-prototype-kit@0.0.1-alpha.3'
+const kitVersion = '0.0.1-alpha.3'
+const kitModule = `govuk-prototype-kit@${kitVersion}`
 
 const npmInstall = (module) => child_process.execSync(`cd ${testDir} && npm install ${module}`, { inherit: true })
 
@@ -16,6 +17,6 @@ const npx = (command) => child_process.execSync(`npx ${kitModule} ${command}`, {
 fs.removeSync(testDir)
 fs.ensureDirSync(testDir)
 fs.writeJsonSync(path.join(testDir, 'usage-data-config.json'), { collectUsageData: false })
-npx('install')
+npx(`install ${kitVersion}`)
 npmInstall('@govuk-prototype-kit/step-by-step@2')
 npx('start')

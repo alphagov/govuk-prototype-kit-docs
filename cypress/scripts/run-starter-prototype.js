fe68890c3fd574528cc7f9f9ab0ef4f535742c1a
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-09-21 14:32:40 +0100

    Change Cypress script to use sync functions
    The promisified version of child_process.exec doesn't support `stdio:
    'inherit'`, and it is really useful to be able to see the output of the
    different steps, so this commit changes the whole script to be
    synchronous, so we can use `execSync`.

--- cypress/scripts/run-starter-prototype.js
+++ cypress/scripts/run-starter-prototype.js
@@ -1,10 +1,7 @@
 const os = require('os')
 const path = require('path')
 const fs = require('fs-extra')
-const { promisify } = require('util')
 const child_process = require('child_process') // eslint-disable-line camelcase
-
-const execPromise = promisify(child_process.exec)
 
 const defaultKitPath = path.join(os.tmpdir(), 'cypress/temp/test-project')
 
@@ -12,14 +9,12 @@ const testDir = path.resolve(process.env.KIT_TEST_DIR || defaultKitPath)
 
 const kitModule = 'govuk-prototype-kit@0.0.1-alpha.3'
 
-const npmInstall = (module) => execPromise(`cd ${testDir} && npm install ${module}`, { inherit: true })
+const npmInstall = (module) => child_process.execSync(`cd ${testDir} && npm install ${module}`, { inherit: true })
 
-const npx = (command) => execPromise(`npx ${kitModule} ${command}`, { cwd: testDir, env: { ...process.env, env: 'test' }, stdio: 'inherit' })
+const npx = (command) => child_process.execSync(`npx ${kitModule} ${command}`, { cwd: testDir, env: { ...process.env, env: 'test' }, stdio: 'inherit' })
 
-;(async () => {
-  await fs.remove(testDir)
-  await fs.ensureDir(testDir)
-  await npx('install')
-  await npmInstall('@govuk-prototype-kit/step-by-step@2')
-  return npx('start')
-})()
+fs.removeSync(testDir)
+fs.ensureDirSync(testDir)
+npx('install')
+npmInstall('@govuk-prototype-kit/step-by-step@2')
+npx('start')
